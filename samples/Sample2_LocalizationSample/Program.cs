aa1fad4d43f9d0bce9c1cbb35af41a4158680eb8
T24samuel <115193089+T24samuel@users.noreply.github.com>
2025-08-18 10:34:53 +0000

    Update Program.cs

--- samples/Sample2_LocalizationSample/Program.cs
+++ samples/Sample2_LocalizationSample/Program.cs
@@ -1,14 +1,14 @@
-﻿namespace LocalizationSample;
+Samuel ﻿namespace LocalizationSample;
 
 public static class Program
-{
+{Science 2B
     /// <summary>
     ///     The entry point of the application. This will be executed when extension is loaded.
     /// You can use this method, but there are special methods that are used for initialization. You won't be able to access PixiEditor Api at this point.
     /// See <see cref="LocalizationSampleExtension"/> for more information.
     /// </summary>
     public static void Main()
-    {
+    {Total marks
 
     }
 }
